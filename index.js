3b323ef31f2d1bb880167d9c796e7f5b84e38967
Seyiodunlade <52614028+seyiodunlade@users.noreply.github.com>
2023-11-11 00:10:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 const input = document.querySelector("#input");
 const words_to_change = document.querySelector("#words_to_change");
 const redactr = document.querySelector("#redactr");
-const output = document.querySelector("#output");
+const output = document.querySelector("#result");
 const getResult = document.querySelector("input[type='button']");
 const other_info = document.querySelector("#other_info");
-const time_used = document.querySelector("#time_used");
-const scanned_words = document.querySelector("#scanned_words");
-const matched_words = document.querySelector("#matched_words");
-const matched_characters = document.querySelector("#matched_characters");
+const time_taken = document.querySelector("#time_taken");
+const scanned_words = document.querySelector("#words_scanned");
+const matched_words = document.querySelector("#words_matched");
+const matched_characters = document.querySelector("#characters_matched");
+const errorsBox = document.querySelector("#errors");
 const pattern1 = /[A-Za-z0-9]+/;
 let characterRedactr = false;
 let censored_words = "";
 let input_copy = "";
 let matching_words = [];
+let resultTabDisabled = true;
+
+//DISABLE EDITABLE PROPERTY OF THE TEXT AREA
+output.disabled = true;
+
+//RESET VARIABLES
+const resetVars = () => {
+  matching_words = [];
+  censored_words = "";
+  errorsBox.innerHTML = "";
+};
+
+// CONTROLLER FOR TAB
+const openTab = (event, tabName) => {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  if (
+    tabName == "result_div" &&
+    event.currentTarget.className.includes("disabled")
+  ) {
+    return;
+  } else {
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+};
+
+document.querySelector("#defaultOpen").click();
 
 // OTHER THINGS TO ADD
 // BETTER CODE STRUCTURE
@@ -30,26 +66,46 @@ const characterMultiplier = (character, n) => {
 };
 
 const wordsMatcher = () => {
-  for (let word of words_to_change.value) {
-    if (word.search) {
+  // console.log("CENSORED WORDS: ", censored_words);
+  for (let censored_word of censored_words) {
+    let pattern = new RegExp(`${censored_word}`, "g");
+    let matches = [...input.value.matchAll(pattern)];
+
+    for (let match of matches) {
+      matching_words.push(match);
     }
   }
+  matched_words.innerHTML = matching_words.length;
+};
+
+const characterMatcher = () => {
+  // return matching_words.join('').length;
+  matched_characters.innerHTML = matching_words.join("").length;
+};
+
+const wordsScanner = () => {
+  return input.value.length;
 };
 
 const runRedact = () => {
+  resetVars();
   var startTime = performance.now();
   if (input.value.length === 0) {
-    console.log("Enter two words or more.");
+    errorsBox.innerHTML = "Enter two words or more.";
+    errorsBox.style.display = "block";
   }
 
   if (words_to_change.value.length === 0) {
-    console.log("Enter a word or words to be censored separated by a comma.");
+    errorsBox.innerHTML =
+      "Enter a word or words to be censored separated by a comma.";
+    errorsBox.style.display = "block";
   } else {
     censored_words = words_to_change.value.split(" ");
   }
 
   if (redactr.value.length === 0) {
     redactr.value = "#";
+    characterRedactr = true;
   } else if (redactr.value.length === 1) {
     characterRedactr = true;
   } else if (redactr.value.length > 1) {
@@ -66,20 +122,28 @@ const runRedact = () => {
       : (censored_result = redactrValue);
 
     const regex = new RegExp(`${word}`, "g");
-    console.log(regex);
+    // console.log(regex);
 
     input_copy = input_copy.replace(regex, censored_result);
   }
 
-  console.log(input_copy);
+  // console.log(input_copy);
+  if (resultTabDisabled === true) {
+    document.querySelector("#resultBtn").classList.remove("disabled");
+  }
+  document.querySelector("#resultBtn").click();
+  output.value = input_copy;
 
   var endTime = performance.now();
   let timeOfProcess = (endTime - startTime) * 0.001;
-  time_used.innerHTML = timeOfProcess.toFixed(4) + "s";
+  time_taken.innerHTML = timeOfProcess.toFixed(4) + "s";
+  scanned_words.innerHTML = wordsScanner();
+  wordsMatcher();
+  characterMatcher();
 
-  console.log((endTime - startTime) * 0.001);
-  console.log("startTime: ", startTime);
-  console.log("endTime: ", endTime);
+  // console.log((endTime - startTime) * 0.001);
+  // console.log("startTime: ", startTime);
+  // console.log("endTime: ", endTime);
 };
 
 getResult.addEventListener("click", runRedact);
